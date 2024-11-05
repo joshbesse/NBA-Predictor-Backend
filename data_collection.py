0797f036e9e65a2ef00862b3e07b309b70da186a
joshbesse <joshua.besse3@gmail.com>
2024-11-04 22:32:04 -0500

    testing nba_api to get necessary data

--- data_collection.py
+++ data_collection.py
@@ -1,14 +1,21 @@
+from nba_api.stats.static import teams
+from nba_api.stats.endpoints import teamgamelog, boxscoreadvancedv2
 import pandas as pd 
-from nba_api.live.nba.endpoints import scoreboard
 
-# today's score board
-scoreboard = scoreboard.ScoreBoard()
-scoreboard_dict = scoreboard.get_dict()
+# team id and names 
+teams = teams.get_teams()
+team_df = pd.DataFrame(teams)[['id', 'full_name']]
 
-# date
-date = scoreboard_dict['scoreboard']['gameDate']
-print(f"date: {date}")
+# season game history for the Hawks -> basic team statistics & game context & team recent performance & target variable 
+games = teamgamelog.TeamGameLog(season='2023-24', season_type_all_star='Regular Season', team_id=1610612737).get_dict()['resultSets']
+games_columns = games[0]['headers']
+games_rows = games[0]['rowSet']
+games_df = pd.DataFrame(data=games_rows, columns=games_columns)
+print(games_df)
 
-# games
-games = scoreboard_dict['scoreboard']['games']
-print(games)
+# get individual player advanced statistics -> need to aggregate -> advanced team statistics & team recent performance  
+boxscore = boxscoreadvancedv2.BoxScoreAdvancedV2(game_id='0022301188')
+advanced = boxscore.get_data_frames()[0]
+#print(advanced[['OFF_RATING', 'DEF_RATING', 'NET_RATING']])
+
+# key player information -> manually collect 
