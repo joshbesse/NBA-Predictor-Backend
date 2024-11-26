37b0268b02b90384dd7d6d9472f5675f6e2232eb
joshbesse <joshua.besse3@gmail.com>
2024-11-25 23:00:53 -0500

    added feature scaling and feature correlation heatmap

--- log_reg.py
+++ log_reg.py
@@ -1,5 +1,21 @@
 import pandas as pd 
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import seaborn as sns 
 
 # load data
 df = pd.read_pickle('./final.pkl')
 
+# feature scaling
+features = df.drop(columns=['Target'])
+scaler = StandardScaler()
+scaled_features = scaler.fit_transform(features)
+scaled_df = pd.DataFrame(scaled_features, columns=features.columns)
+scaled_df['Target'] = df['Target']
+
+# check correlation
+corr_matrix = df.corr()
+plt.figure(figsize=(12, 8))
+sns.heatmap(corr_matrix, annot=False, cmap='coolwarm', cbar=True)
+plt.title("Feature Correlation Heatmap")
+plt.show()
