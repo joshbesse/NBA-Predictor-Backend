3a0d306993e1bf6ae8e9ca799af1090ba8c49a31
joshbesse <joshua.besse3@gmail.com>
2024-11-26 21:54:32 -0500

    split data into training, validation, and test sets

--- log_reg.py
+++ log_reg.py
@@ -19,3 +19,19 @@ plt.figure(figsize=(12, 8))
 sns.heatmap(corr_matrix, annot=False, cmap='coolwarm', cbar=True)
 plt.title("Feature Correlation Heatmap")
 plt.show()
+# rolling averages like ppg, fg_pct, off_rating, etc. are highly correlated with itself (ppg_home and fg_pct home) and with other team (ppg_away, reb_home)
+# going to use ridge logistic regression (l2 regularization)
+
+# split the data into training, validation, and testing sets
+# need validation set for tuning regularization parameter
+# need to split chronologically to avoid leaking future information into past predictions 
+train_split = int(len(scaled_df) * 0.70)
+val_split = int(len(scaled_df) * 0.85)
+
+train_df = scaled_df[:train_split]
+val_df = scaled_df[train_split:val_split]
+test_df = scaled_df[val_split:]
+
+X_train, y_train = train_df.drop(columns=['Target']), train_df['Target']
+X_val, y_val = val_df.drop(columns=['Target']), val_df['Target']
+X_test, y_test = test_df.drop(columns=['Target']), test_df['Target']
